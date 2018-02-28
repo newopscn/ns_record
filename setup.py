e7ead55e7bf0f393de164240f44942c142651a94
wangsir <wanglin@dbca.cn>
2018-02-28 07:58:40 +0000

    add comma to "print IPT.my_wan_ip()", output "print IPT.my_wan_ip(),"; dont close with \n

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='ns_record',
-    version='1.0.6',
+    version='1.0.7',
     license="GPLv2",
     author='wangsir',
     author_email='wanglin@dbca.cn',
